921cdaf86ba49d3be58fb869fdb0f5d22fb48a82
premganeshkrify <premganesh@krify.net>
2022-07-27 14:52:26 +0530

    javascript

---
+++ html/helloworld.js
@@ -0,0 +1,4 @@
+function name(name, id) {
+    return ` My name is ${name} my id is ${id}`
+}
+console.log(name('prem', 4))
---
+++ html/myScript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    const name = 'hello world';
+    document.getElementById("demo").innerHTML = `Paragraph this ${name} is prem ganesh changed.`;
+}
+
